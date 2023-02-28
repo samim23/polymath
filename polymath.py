5251756c92d0a2264ff0d1dc3f77b1ed182188c4
Damon Hayhurst <damonhayhurst@gmail.com>
2023-02-28 23:39:31 +0000

    remove print

--- polymath.py
+++ polymath.py
@@ -132,7 +132,6 @@ def audio_process(vids, videos):
         print('------ process audio',vid)
         # extract file name
         audioname = vid.split("/")[-1]
-        print(audioname)
         audioname, _ = os.path.splitext(audioname)
 
         # generate a unique ID based on file path and name
